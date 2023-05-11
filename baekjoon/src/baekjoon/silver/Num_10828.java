a93f94f6c5f8fae5c2349c5253f3a4051fbd675c
Jayyyyp <pjae9726@gmail.com>
2023-05-11 23:15:50 +0900

    커밋

---
+++ baekjoon/src/baekjoon/silver/Num_10828.java
@@ -0,0 +1,43 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num_10828 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine()); // 명령의 수
+		int[] stack = new int[N];
+		
+		
+		int command = Integer.parseInt(br.readLine()); // 명령들
+		
+		br.close();
+
+	}
+	
+	public void push(){
+		
+	}
+	
+	public void pop() {
+		
+	}
+	
+	public void size() {
+		
+	}
+	
+	public void empty() {
+		
+	}
+	
+	public void top() {
+		
+	}
+
+}
