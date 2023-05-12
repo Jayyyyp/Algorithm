726e36ae1e5caac63dcc7ddc5b2d30e815d57708
Jayyyyp <pjae9726@gmail.com>
2023-05-12 15:58:15 +0900

    5/12 커밋

---
+++ baekjoon/src/baekjoon/bronze/Num_2577.java
@@ -0,0 +1,24 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Num_2577 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		br.close();
+		int result = A * B * C;
+		
+		
+		
+	}
+
+}
