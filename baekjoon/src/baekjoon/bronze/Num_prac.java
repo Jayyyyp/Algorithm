d311943976680acd857634d66dd88ab7530291af
Jayyyyp <pjae9726@gmail.com>
2023-05-13 09:48:56 +0900

    stack

---
+++ baekjoon/src/baekjoon/bronze/Num_prac.java
@@ -0,0 +1,10 @@
+package baekjoon.bronze;
+
+public class Num_prac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- baekjoon/src/baekjoon/silver/Num_10828.java
+++ baekjoon/src/baekjoon/silver/Num_10828.java
@@ -1,43 +1,78 @@
 package baekjoon.silver;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Num_10828 {
-
-	public static void main(String[] args) throws IOException {
-		
+	static int[] stack;
+	static int size = 0;
+	
+	public static void push(int num) {
+		stack[size] = num;
+		size++;
+	}
+	public static int pop() {
+		if(size == 0) {
+			return -1;
+		}else {
+			int outNum = stack[size-1];
+			stack[size-1] = 0;
+			size--;
+			return outNum;
+		}
+	}
+	public static int size() {
+		return size;
+	}
+	public static int empty() {
+		if(size == 0) {
+			return 1;
+		}else {
+			return 0;
+		}
+	}
+	public static int top() {
+		if(size == 0) {
+			return -1;
+		}else {
+			return stack[size-1];
+		}
+	}
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int N = Integer.parseInt(br.readLine()); // 명령의 수
-		int[] stack = new int[N];
+		int N = Integer.parseInt(br.readLine());
+		stack = new int[N];
 		
+		for(int i = 1; i <= N; i++) {
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int command = Integer.parseInt(br.readLine()); // 명령들
-		
+		switch(st.nextToken()) {
+		case "push":
+			push(Integer.parseInt(st.nextToken()));
+			break;
+		case "pop":
+			bw.write(pop() + "\n");
+			break;
+		case "size":
+			bw.write(size() + "\n");
+			break;
+		case "empty":
+			bw.write(empty() + "\n");
+			break;
+		case "top":
+			bw.write(top() + "\n");
+			break;
+		}
+	}
 		br.close();
-
+		bw.flush();
+		bw.close();
 	}
-	
-	public void push(){
-		
-	}
-	
-	public void pop() {
-		
-	}
-	
-	public void size() {
-		
-	}
-	
-	public void empty() {
-		
-	}
-	
-	public void top() {
-		
-	}
-
 }
+
